0f6bbd507cb4c365b23de1a7146c060005ff1b67
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-11 10:59:48 -0500

    Add CatchAll.

--- src/main/kotlin/org/aaron/TestServer.kt
+++ src/main/kotlin/org/aaron/TestServer.kt
@@ -3,19 +3,22 @@ package org.aaron
 import org.aaron.routes.CommandsRoute
 import org.aaron.routes.HealthRoute
 import org.aaron.routes.RequestInfoRoute
-import org.http4k.core.HttpHandler
-import org.http4k.core.Uri
-import org.http4k.core.then
+import org.http4k.core.*
 import org.http4k.events.*
 import org.http4k.filter.ResponseFilters
+import org.http4k.filter.ServerFilters
 import org.http4k.format.Jackson
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
+import java.io.PrintWriter
+import java.io.StringWriter
 import java.util.concurrent.atomic.AtomicLong
 
 // this is our custom event which will be printed in a structured way
 data class IncomingHttpRequest(val uri: Uri, val status: Int, val duration: Long) : Event
+
+data class CatchAllExceptionEvent(val stackTrace: String) : Event
 
 data class ServerStartedEvent(val port: Int) : Event
 
@@ -37,6 +40,16 @@ fun main() {
         RequestInfoRoute(),
     )
 
+    val catchAll = ServerFilters.CatchAll { error ->
+        val stackTraceAsString = StringWriter().apply {
+            error.printStackTrace(PrintWriter(this))
+        }.toString()
+
+        events(CatchAllExceptionEvent(stackTrace = stackTraceAsString))
+
+        Response(Status.INTERNAL_SERVER_ERROR)
+    }
+
     val appWithEvents =
         ResponseFilters.ReportHttpTransaction {
             // to "emit" an event, just invoke() the Events!
@@ -47,7 +60,8 @@ fun main() {
                     duration = it.duration.toMillis(),
                 )
             )
-        }.then(app)
+        }.then(catchAll)
+            .then(app)
 
     val server = appWithEvents.asServer(Undertow(port = 8080)).start()
 

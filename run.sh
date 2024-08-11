193c2c1a3a207a729dfbe4ba5359a43f976edf7a
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-11 06:01:24 -0500

    Update PATH.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export PATH=/home/aaron/.sdkman/candidates/java/current/bin/:$PATH
+export PATH=/home/aaron/.sdkman/candidates/java/current/bin:$PATH
+export PATH=/home/aaron/bin:$PATH
 
 java -Xmx512m -XX:+UseZGC -XX:+ZGenerational -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar 2>&1 | simplerotate logs

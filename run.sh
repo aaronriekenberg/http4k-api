44074605e9e19356c258b5aa3a1c11b4d368003f
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-11 05:39:18 -0500

    Use simplerotate logs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx512m -XX:+UseZGC -XX:+ZGenerational -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar
+java -Xmx512m -XX:+UseZGC -XX:+ZGenerational -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar 2>&1 | simplerotate logs

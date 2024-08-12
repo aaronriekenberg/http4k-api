02bf700390bcec58b39e9ed2f8d49d032b6bb73a
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-12 16:58:51 -0500

    Add -XX:+AlwaysPreTouch.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 export PATH=/home/aaron/.sdkman/candidates/java/current/bin:$PATH
 export PATH=/home/aaron/bin:$PATH
 
-java -Xms512m -Xmx512m -XX:+UseZGC -XX:+ZGenerational -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar 2>&1 | simplerotate logs
+java -Xms512m -Xmx512m -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar 2>&1 | simplerotate logs

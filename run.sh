dad77b40150adae51a4aa14cec9d1c410bccc86a
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-18 13:42:26 -0500

    Do not use simplerotate for logs.

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 export PATH=/home/aaron/.sdkman/candidates/java/current/bin:$PATH
-export PATH=/home/aaron/bin:$PATH
 
-java -Xms1g -Xmx1g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar 2>&1 | simplerotate logs
+exec java -Xms1g -Xmx1g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar

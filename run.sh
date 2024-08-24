f618c3d3053d467ca454117aeec5c52187a87afe
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-24 11:42:16 -0500

    Add PORT and HTTP2_ENABLED.

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 export PATH=/home/aaron/.sdkman/candidates/java/current/bin:$PATH
 
+export PORT=8080
+export HTTP2_ENABLED=true
+
 exec java -Xms1g -Xmx1g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar

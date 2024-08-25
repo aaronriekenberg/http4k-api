14cd97ec7756c185d7db7d266dde482660ec9d55
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-25 04:53:38 -0500

    Add comment.

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ export PATH=/home/aaron/.sdkman/candidates/java/current/bin:$PATH
 export PORT=8080
 export HTTP2_ENABLED=true
 
+# use -Xlog:gc*:gc.log for verbose GC logs
 exec java -Xms1g -Xmx1g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar

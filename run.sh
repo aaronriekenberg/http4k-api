e02c2c65e8fa81877fed8291caa5f7317cc24a89
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-08-30 04:19:22 -0500

    2g heap size.

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export PORT=8080
 export HTTP2_ENABLED=true
 
 # use -Xlog:gc*:gc.log for verbose GC logs
-exec java -Xms1g -Xmx1g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar
+exec java -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar

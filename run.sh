f130c54aa52b208a3b1dbc44e723b4954135f5d9
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-09-01 04:18:50 -0500

    Disable verbose gc logs.

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export PORT=8080
 export HTTP2_ENABLED=true
 
 # use -Xlog:gc*:gc.log for verbose GC logs
-exec java -Xms4g -Xmx4g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -XX:+UseTransparentHugePages -Xlog:gc*:gc.log -jar ./build/libs/http4k-api.jar
+exec java -Xms4g -Xmx4g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -XX:+UseTransparentHugePages -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar

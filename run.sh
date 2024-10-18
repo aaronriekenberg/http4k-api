ee686b2a00602bdac5c2627f851f772f4d5adfc5
Aaron.Riekenberg <aaron.riekenberg@target.com>
2024-10-18 16:05:03 -0500

    Remove unused variable.

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 export PATH=/home/aaron/.sdkman/candidates/java/current/bin:$PATH
 
 export PORT=8080
-export HTTP2_ENABLED=true
 
 # use -Xlog:gc*:gc.log for verbose GC logs
 #exec java -Xms4g -Xmx4g -XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -XX:+UseTransparentHugePages -Xlog:gc:gc.log -jar ./build/libs/http4k-api.jar
